026ee80a27840588795f12fe6cbd1355cfe477c6
JD Smith <93749+jdtsmith@users.noreply.github.com>
2023-07-26 21:45:40 -0400

    commentary: improve

--- indent-bars.el
+++ indent-bars.el
@@ -24,11 +24,14 @@
 
 ;;; Commentary:
 
-;; indent-bars highlights indentation with configurable font-based
-;; bars.  It includes the option for depth-varying colors and
-;; highlighting the indentation level of the current line.  Bars span
-;; blank lines, by default.  Works only for modes using fixed,
-;; space-based indentation.
+;; indent-bars highlights indentation with configurable font-lock
+;; based vertical bars.  The color and appearance (weight, pattern,
+;; position within the character, zigzag, etc.) are all configurable.
+;; Includes the option for depth-varying colors and highlighting the
+;; indentation level of the current line.  Bars span blank lines, by
+;; default.  indent-bars works in any mode using fixed, space-based
+;; indentation.
+
 
 ;;; Code:
 ;;;; Requires
