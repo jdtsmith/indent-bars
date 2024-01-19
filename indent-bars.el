6e2f93d1949fffc9eb7f6f88f744c7910ffa34aa
JD Smith <93749+jdtsmith@users.noreply.github.com>
2024-01-19 14:58:03 -0500

    ib: improve docs and add shorthands

--- indent-bars.el
+++ indent-bars.el
@@ -1,4 +1,4 @@
-;;; indent-bars.el --- highlight indentation with bars -*- lexical-binding: t; -*-
+;;; indent-bars.el --- Highlight indentation with bars -*- lexical-binding: t; -*-
 ;; Copyright (C) 2023  J.D. Smith
 
 ;; Author: J.D. Smith
@@ -33,6 +33,28 @@
 ;; or space-based indentation.  In the terminal (or on request) it
 ;; uses vertical bar characters instead of stipple patterns.
 
+;; For Developers:
+;;
+;; To efficiently accommodate simultaneous alternative bar styling, we
+;; do two things:
+;;
+;;  1. Collect all the style related information (color, stipple
+;;     pattern, etc.) into a single struct, operating on one such
+;;     "current" style struct at a time.
+;;
+;;  2. Provide convenience functions for replicating "alternative"
+;;     custom variables the user can configure; see
+;;     `indent-bars--style'.  These variables can "inherit" nil or
+;;     omitted plist variables from their parent var.
+;;
+;; To temporarily alter the current style, it's enough to bind the
+;; variable `indent-bars-current-style' dynamically.
+;;
+;; Note the shorthand substitutions for style related prefixes (slot
+;; accessors and variable; see file-local-variables at the end):
+;; 
+;;    ibs/  => indent-bars-style-
+;;    ibcs  => indent-bars-current-style
 
 ;;; Code:
 ;;;; Requires
@@ -1162,3 +1184,6 @@ Adapted from `highlight-indentation-mode'."
 (provide 'indent-bars)
 
 ;;; indent-bars.el ends here
+;; Local Variables:
+;; read-symbol-shorthands: (("ibs/" . "indent-bars-style-") ("ibcs" . "indent-bars-current-style"))
+;; End:
