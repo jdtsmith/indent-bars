eca496b2db240e6bc92029f9fdbb856a130e6821
JD Smith <93749+jdtsmith@users.noreply.github.com>
2024-06-07 15:01:17 -0400

    improve commentary

--- indent-bars.el
+++ indent-bars.el
@@ -24,14 +24,14 @@
 
 ;;; Commentary:
 
-;; indent-bars highlights indentation with configurable font-lock
-;; based vertical bars, using stipples.  The color and appearance
-;; (weight, pattern, position within the character, zigzag, etc.) are
-;; all configurable.  Includes the option for depth-varying colors and
-;; highlighting the indentation level of the current line.  Bars span
-;; blank lines, by default.  indent-bars works in any mode using fixed tab
-;; or space-based indentation.  In the terminal (or on request) it
-;; uses vertical bar characters instead of stipple patterns.
+;; indent-bars highlights indentation with configurable vertical bars,
+;; using stipples.  The color and appearance (weight, pattern,
+;; position within the character, zigzag, etc.) are all configurable.
+;; Options include depth-varying colors and highlighting the
+;; indentation depth of the current line.  Bars span blank lines, by
+;; default.  indent-bars works in any mode using fixed tab or
+;; space-based indentation.  In the terminal (or on request) it uses
+;; vertical bar characters instead of stipple patterns.
 
 ;; For Developers:
 ;;
@@ -43,12 +43,9 @@
 ;;     "current" style struct at a time.
 ;;
 ;;  2. Provide convenience functions for replicating "alternative"
-;;     custom variables the user can configure; see
+;;     custom style variables the user can configure; see
 ;;     `indent-bars--style'.  These variables can "inherit" nil or
 ;;     omitted plist variables from their parent var.
-;;
-;; To temporarily alter the current style, it's enough to bind the
-;; variable `indent-bars-current-style' dynamically.
 ;;
 ;; Note the shorthand substitution for style related slot;
 ;; see file-local-variables at the end:
