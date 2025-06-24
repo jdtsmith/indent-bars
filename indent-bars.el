9a89849cec54debc85791118ea9b05cf9567765f
JD Smith <93749+jdtsmith@users.noreply.github.com>
2025-06-24 07:59:41 -0400

    Bump version and document

--- indent-bars.el
+++ indent-bars.el
@@ -4,7 +4,7 @@
 ;; Author: J.D. Smith <jdtsmith+elpa@gmail.com>
 ;; Homepage: https://github.com/jdtsmith/indent-bars
 ;; Package-Requires: ((emacs "27.1") (compat "30"))
-;; Version: 0.8.4
+;; Version: 0.8.5
 ;; Keywords: convenience
 
 ;; indent-bars is free software: you can redistribute it and/or
