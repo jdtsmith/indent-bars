77b3a117e8ba4b90459a3d9c0873dbfcbe3456ae
JD Smith <93749+jdtsmith@users.noreply.github.com>
2025-06-24 07:59:05 -0400

    Add TCL mode support

--- indent-bars.el
+++ indent-bars.el
@@ -1651,6 +1651,8 @@ Adapted from `highlight-indentation-mode'."
     sh-basic-offset)
    ((and (derived-mode-p 'java-ts-mode) (boundp 'java-ts-mode-indent-offset))
     java-ts-mode-indent-offset)
+   ((and (derived-mode-p 'tcl-mode) (boundp 'tcl-indent-level))
+    tcl-indent-level)
    ((and (boundp 'standard-indent) standard-indent))
    (t 4))) 				; backup
 
